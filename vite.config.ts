3ac3ca46d102e743f02ec2c5f5da60ec8976fbbb
chenmanni0910 <647804323@qq.com>
2026-03-15 18:58:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 新增这一行，必须和仓库名一致
+  base: '/my-food-diary-01/',
   server: {
     host: "::",
     port: 8080,
